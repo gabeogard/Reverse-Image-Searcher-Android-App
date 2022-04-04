3769c98662690d58f8c6358c127e2c55081117ad
gabeogard <gaog001@student.kristiania.no>
2022-04-04 12:56:16 +0200

    Init commit - Added dependencies for API calls and for parsing JSON. Added viewbinding to main activity.

--- app/src/main/java/no/kristiania/imagesearcherexam/MainActivity.kt
+++ app/src/main/java/no/kristiania/imagesearcherexam/MainActivity.kt
@@ -2,10 +2,14 @@ package no.kristiania.imagesearcherexam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import no.kristiania.imagesearcherexam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
     }
 }
